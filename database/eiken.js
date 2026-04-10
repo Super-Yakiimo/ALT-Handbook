f74233ca14cf7ebfc29b6c3f9d25b401ff6e0bf3
InnovationJamez <41845456+InnovationJamez@users.noreply.github.com>
2026-04-10 23:00:43 +0900

    eiken three

---
+++ database/eiken.js
@@ -0,0 +1,265 @@
+// questions
+const N5 = [
+    {
+        text: "He's ( ) his dog in the garden.",
+        options: ["reading", "washing", "making", "sitting"],
+        answer: "washing"
+    },
+    {
+        text: "It's very cold, Billy. () a coat today.",
+        options: ["Listen", "Wear", "See", "Watch"],
+        answer: "Wear"
+    },
+    {
+        text: "I have a photo (). I have many pictures of my family in it.",
+        options: ["album", "pen", "sun", "dog"],
+        answer: "album"
+    },
+    {
+        text: "My brother and I () to school by bus every day.",
+        options: ["talk", "eat", "see", "come"],
+        answer: "come"
+    },
+    {
+        text: "I love basketball. I often watch basketball games () TV with my brother.",
+        options: ["of", "on", "at", "in"],
+        answer: "on"
+    },
+    {
+        text: "Tom has a () of coffee every morning before he goes to school.",
+        options: ["corn", "floor", "window", "cup"],
+        answer: "cup"
+    },
+    {
+        text: "I get () early every morning and go to the park.",
+        options: ["on", "up", "over", "right"],
+        answer: "up"
+    },
+    {
+        text: "Janet () swimming every Sunday with her sister.",
+        options: ["goes", "asks", "buys", "drinks"],
+        answer: "goes"
+    },
+    {
+        text: "Mom, can I () a letter to Uncle Rob this afternoon?",
+        options: ["writes", "wrote", "write", "writing"],
+        answer: "write"
+    },
+    {
+        text: "I know Tim and Lucy. () are Canadian.",
+        options: ["He", "They", "It", "You"],
+        answer: "They"
+    }
+];
+
+const N4 = [
+    {
+        text: "Kentaro wants to be a ( ) baseball player in the future. He wants to play in the United States someday.",
+        options: ["difficult", "professional", "strange", "thick"],
+        answer: "professional"
+    },
+    {
+        text: "It'll rain this evening, Peter. Don't forget your ().",
+        options: ["umbrella", "dream", "mirror", "sandwich"],
+        answer: "umbrella"
+    },
+    {
+        text: "Yuta loves flying. He often goes to the ( ) to watch the planes.",
+        options: ["floor", "hospital", "airport", "mall"],
+        answer: "airport"
+    },
+    {
+        text: "Takeru's bag has many large books in it. It is very ( ), so Takeru can't carry it.",
+        options: ["heavy", "nervous", "late", "fast"],
+        answer: "heavy"
+    },
+    {
+        text: "Yumi visited a ( ) last weekend. She likes learning about history.",
+        options: ["dictionary", "question", "castle", "cloud"],
+        answer: "castle"
+    },
+    {
+        text: "Peter grows potatoes in his ( ). He also has an apple tree.",
+        options: ["cake", "song", "meat", "garden"],
+        answer: ""
+    },
+    {
+        text: "Last week, Brian () a cold. He stayed in bed for two days.",
+        options: ["caught", "told", "forgot", "played"],
+        answer: "caught"
+    },
+    {
+        text: "It was very hot during soccer practice. After practice, Jeff drank a ( ) of water.",
+        options: ["glass", "boat", "country", "floor"],
+        answer: "glass"
+    },
+    {
+        text: "Last week, a new student came to Lucy’s school. Lucy became good friends ( ) him because they both love sports.",
+        options: ["with", "through", "by", "into"],
+        answer: "with"
+    },
+    {
+        text: "Kate can ( ) very well. She’s on the school swimming team.",
+        options: ["swam", "swim", "swims", "to swim"],
+        answer: "swim"
+    }
+]
+
+const N3 = [
+    {
+        text: "A: Mom, I ate my sandwich, but I’m still hungry. B: OK. Wait a minute. I’ll make you ( ) one.",
+        options: ["another", "near", "every", "much"],
+        answer: ""
+    },
+    {
+        text: "A: Do you know where the library is? B: It’s ( ) the post office.",
+        options: ["during", "of", "until", "beside"],
+        answer: "beside"
+    },
+    {
+        text: "( ) by scientists shows that global warming is a big problem.",
+        options: ["Research", "Energy", "Environment", "Danger"],
+        answer: "Research"
+    },
+    {
+        text: "John has an ( ) tomorrow. He hopes to get a new job.",
+        options: ["interview", "airport", "example", "excuse"],
+        answer: "interview"
+    },
+    {
+        text: "The road was closed yesterday because there was an ( ).",
+        options: ["accident", "appointment", "apartment", "address"],
+        answer: ""
+    },
+    {
+        text: "A: What should I do with this ( ) bottle? B: We can recycle it.",
+        options: ["glad", "empty", "different", "serious"],
+        answer: "empty"
+    },
+    {
+        text: "This morning, Pat took a ( ) medicine for his stomachache, but it didn’t get better. He took some more later.",
+        options: ["little", "lot", "most", "much"],
+        answer: ""
+    },
+    {
+        text: "A: Can you ( ) down the radio, please? B: OK.",
+        options: ["turn", "get", "go", "make"],
+        answer: "turn"
+    },
+    {
+        text: "A: Where do you want to meet, Kate? B: Let’s meet ( ) front of the flower shop.",
+        options: ["on", "for", "in", "to"],
+        answer: ""
+    },
+    {
+        text: "Mr. Ford often buys a newspaper at the station in the morning ( ) his way to work.",
+        options: ["at", "by", "on", "to"],
+        answer: "on"
+    }
+]
+
+const P2 = [
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    }
+]
+
+const N2 = [
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        text: "",
+        options: ["", "", "", ""],
+        answer: ""
+    }
+]
