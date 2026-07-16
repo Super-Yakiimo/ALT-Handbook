f87db96ee397de02a20f362e87be6b5be1db1534
InnovationJamez <41845456+InnovationJamez@users.noreply.github.com>
2026-07-16 12:02:18 +0900

    paper gen made

--- study/print-maker/printMaker.js
+++ study/print-maker/printMaker.js
@@ -3,8 +3,63 @@ const WIDTH = 5;
 const HEIGHT = 2;
 const comicPath = "./old/comicteacher.png";
 
-function start() {
+const TSTART = '<table>';
+const TEND = '</table>'
 
+
+const RSTART = '<tr>';
+const REND = '</tr>'
+
+function makeDict(vocab) {
+    let dictString = '<table id="dict">';
+
+    // make dictionary
+    for (let j = 0; j < HEIGHT; j++) {
+        dictString += RSTART;
+        for (let i = 0; i < WIDTH; i++) {
+            // add row
+            let obj = vocab[j * 5 + i];
+            let path = "../../resource/img/" + obj.link;
+            let tds = `<td><img src="${path}"><p>${obj.name}</p></td>`;
+            dictString += tds;
+        }
+        dictString += REND;
+    }
+
+    dictString += TEND;
+
+    return dictString;
+}
+
+function makeWords(vocab, quest, resp) {
+    let result = "<div id='words'>";
+    vocab.forEach(obj => {
+        // add words
+        result += `<p class="trace">${quest}</p><p class="trace">${resp} ${obj.name}</p>`;
+    });
+    result += "</div>";
+    return result;
+}
+
+function makePuzzle(vocab, quest, resp) {
+    let puzzle = '<table id="puzzle">';
+
+    // make puzzle
+    for (let j = 0; j < WIDTH; j++) {
+        puzzle += TSTART;
+        for (let i = 0; i < HEIGHT; i++) {
+            let obj = vocab[i * WIDTH + j];
+            let path = "../../resource/img/" + obj.link;
+            puzzle += `<td><img src="${comicPath}"><p class='quest'>${quest}</p><p class='trace'>${resp} ______.</p><img class="corner" src="${path}"></td>`
+        }
+        puzzle += REND;
+    }
+
+    puzzle += TEND;
+    return puzzle;
+}
+
+function start() {
     let vocab = getVocab(false);
 
     // return alert if none selected
@@ -15,50 +70,43 @@ function start() {
     let quest = document.querySelector('#quest').value;
     let resp = document.querySelector('#answer').value;
 
-    let table = document.querySelector("#table");
-    let words = document.querySelector("#words");
-    let puzzle = document.querySelector("#puzzle");
+    let printArea = document.querySelector("#printArea");
+    let editBox = document.querySelector("#editBox");
+    
+    let htmlCon = "";
+
+    // dictionary
+    htmlCon += "\n<h1>Dictionary</h1>\n"
+    htmlCon += makeDict(vocab);
+
+    // tracing words
+    htmlCon += "\n<h1>Tracing</h1>\n"
+    htmlCon += makeWords(vocab, quest, resp);
+
+    // puzzle 
+    htmlCon += "\n<h1>Puzzle</h1>\n"
+    htmlCon += makePuzzle(vocab, quest, resp);
+
+    // add to print preview
+    printArea.innerHTML = htmlCon; 
+    editBox.value = htmlCon;
+}
 
 
-    document.querySelector('.control').classList.add('hide');
+function edit(){
+    let editBox = document.querySelector("#editBox");
+    let printArea = document.querySelector("#printArea");
 
-
-    // make dictionary
-    for (let j = 0; j < HEIGHT; j++) {
-        let tr = document.createElement('tr');
-        for (let i = 0; i < WIDTH; i++) {
-            // add row
-            let td = document.createElement('td');
-            let obj = vocab[j * 5 + i];
-            let path = "../../resource/img/" + obj.link;
-            td.innerHTML = `<img src="${path}"><p>${obj.name}</p>`;
-            tr.appendChild(td);
-
-            // add words
-            words.innerHTML += `<p>${quest}</p><p>${resp} ${obj.name}</p>`;
-
-        }
-        table.appendChild(tr);
+    if(editBox.innerHTML = ""){
+        return;
     }
 
-    // make words
+    printArea.innerHTML = editBox.value;
+}
 
-
-    // make puzzle
-    for (let j = 0; j < WIDTH; j++) {
-        let tr = document.createElement('tr');
-        for (let i = 0; i < HEIGHT; i++) {
-            let obj = vocab[i * WIDTH + j];
-            let path = "../../resource/img/" + obj.link;
-
-            // add row
-            let td = document.createElement('td');
-            td.innerHTML = `<img src="${comicPath}"><p class='quest'>${quest}</p><p class='trace'>${resp} ______.</p><img class="corner" src="${path}">`;
-
-            tr.appendChild(td);
-        }
-        puzzle.appendChild(tr);
-    }
+function printPreview(){
+    let printArea = document.querySelector("#printArea");
+    print()
 }
 
 window.onload = function () {
